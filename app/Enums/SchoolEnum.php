1e0eccb6f26328ad63989e6b90812c0d27c1668b
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-08-27 23:15:03 +0000

    Create Models, migrations and factories

---
+++ app/Enums/SchoolEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Enums;
+
+
+enum SchoolEnum : string
+{
+    CASE SCHOOL_1 = 'school_1';
+    CASE SCHOOL_2 = 'school_2';
+    CASE SCHOOL_3 = 'school_3';
+}

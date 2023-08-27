9f79aaff6df2a1c3c7cbc1118d422d7bbaf3512d
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-08-27 15:29:14 +0000

    Install pest package for testing and create our first test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,8 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+it('has a new welcome page', function(){
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    $this->get('/')->assertOk();
 
-        $response->assertStatus(200);
-    }
-}
+});
